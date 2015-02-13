7f5056c8918c379f39a2b1ca2319ac771ecc4d7b
Travis McMahon <azgoalie@gmail.com>
2015-02-13 03:12:47 -0700

    Added user model

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get  'home', to: 'static_pages#home'
   get  'about', to: 'static_pages#about'
-  get 'signup', to:'users#new'
+  get  'signup', to:'users#new'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
