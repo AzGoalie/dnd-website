c1d68553aa145ab97fb99fbd1efe97bfbeddb23a
Travis McMahon <azgoalie@gmail.com>
2015-02-13 19:19:13 -0700

    finished user signup

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Layout gems
 gem 'foundation-rails'
+gem 'simple_form'
 
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get  'home', to: 'static_pages#home'
   get  'about', to: 'static_pages#about'
   get  'signup', to:'users#new'
+  
+  resources :users
 
 
   # The priority is based upon order of creation: first created -> highest priority.
