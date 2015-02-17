2a30d091646181d686dab3f8f6327e13e1f4244e
Travis McMahon <azgoalie@gmail.com>
2015-02-16 19:54:49 -0700

    Changed email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: "noreply@example.com"
+  default from: "noreply@df-app.herokuapp.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
