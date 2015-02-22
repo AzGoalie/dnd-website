0d9f4113a124c91c5ce0f0fb7419911a398c27d6
Travis McMahon <azgoalie@gmail.com>
2015-02-22 02:31:32 -0700

    removed foreign key

--- db/migrate/20150221093346_add_characters_ref_to_campaigns.rb
+++ db/migrate/20150221093346_add_characters_ref_to_campaigns.rb
@@ -1,6 +1,5 @@
 class AddCharactersRefToCampaigns < ActiveRecord::Migration
   def change
     add_reference :campaigns, :characters, index: true
-    add_foreign_key :campaigns, :characters
   end
 end
